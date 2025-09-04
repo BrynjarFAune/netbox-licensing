027ff1d09e72cd3a31d71e92bdef2fb2e4c55999
Brynjar <brynjarfaune@gmail.com>
2025-09-04 11:11:19 +0200

    fix: restore original working navigation structure with lists (NetBox 4.3.7)

--- netbox_licenses/navigation.py
+++ netbox_licenses/navigation.py
@@ -1,31 +1,65 @@
 from netbox.plugins import PluginMenuItem, PluginMenuButton, PluginMenu
+from netbox.choices import ButtonColorChoices
 
-# Create buttons using proper PluginMenuButton class
-protected_resource_buttons = (
+license_buttons = [
     PluginMenuButton(
         link='plugins:netbox_licenses:license_add',
-        title='Add License',
-        icon_class='mdi mdi-plus-thick'
-    ),
-)
+        title='Add',
+        icon_class='mdi mdi-plus-thick',
+    )
+]
 
-# Create the menu with proper navigation structure
+licenseinstance_buttons = [
+    PluginMenuButton(
+        link='plugins:netbox_licenses:licenseinstance_add',
+        title='Add',
+        icon_class='mdi mdi-plus-thick',
+    )
+]
+
 menu = PluginMenu(
-    label='License Control',
-    groups=(
-        ('Overview', (
+    label='licenses',
+    groups=[
+        ('licenses', [
             PluginMenuItem(
                 link='plugins:netbox_licenses:license_list',
-                link_text='Licenses'
+                link_text='Licenses',
+                buttons=license_buttons
             ),
-        )),
-        ('Management', (
             PluginMenuItem(
-                link='plugins:netbox_licenses:license_list',
-                link_text='License Management',
-                buttons=protected_resource_buttons
+                link='plugins:netbox_licenses:licenseinstance_list',
+                link_text='License Instances',
+                buttons=licenseinstance_buttons
             ),
-        )),
-    ),
-    icon_class='mdi mdi-shield-lock'
+        ]),
+        ('reports', [
+            PluginMenuItem(
+                link='plugins:netbox_licenses:utilization_report',
+                link_text='Utilization Report'
+            ),
+            PluginMenuItem(
+                link='plugins:netbox_licenses:vendor_utilization',
+                link_text='Vendor Analysis'
+            ),
+        ]),
+        ('analytics', [
+            PluginMenuItem(
+                link='plugins:netbox_licenses:license_analytics',
+                link_text='License Analytics'
+            ),
+            PluginMenuItem(
+                link='plugins:netbox_licenses:compliance_monitoring',
+                link_text='Compliance Monitoring'
+            ),
+            PluginMenuItem(
+                link='plugins:netbox_licenses:cost_allocation',
+                link_text='Cost Allocation'
+            ),
+            PluginMenuItem(
+                link='plugins:netbox_licenses:license_renewals',
+                link_text='License Renewals'
+            ),
+        ])
+    ],
+    icon_class='mdi mdi-certificate'
 )
