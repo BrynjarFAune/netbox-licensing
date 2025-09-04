2936605d3d9e2e67b5da8b6fa8d7a47e37dc94f0
Brynjar <brynjarfaune@gmail.com>
2025-09-04 11:00:32 +0200

    test: copy exact Azure Groups navigation pattern structure

--- netbox_licenses/navigation.py
+++ netbox_licenses/navigation.py
@@ -1,15 +1,31 @@
-from netbox.plugins import PluginMenuItem, PluginMenu
+from netbox.plugins import PluginMenuItem, PluginMenuButton, PluginMenu
 
-# Minimal test navigation - single menu item to isolate issue
+# Create buttons using proper PluginMenuButton class
+protected_resource_buttons = (
+    PluginMenuButton(
+        link='plugins:netbox_licenses:license_add',
+        title='Add License',
+        icon_class='mdi mdi-plus-thick'
+    ),
+)
+
+# Create the menu with proper navigation structure
 menu = PluginMenu(
-    label='Licenses',
+    label='License Control',
     groups=(
-        ('Test', (
+        ('Overview', (
             PluginMenuItem(
                 link='plugins:netbox_licenses:license_list',
                 link_text='Licenses'
             ),
         )),
+        ('Management', (
+            PluginMenuItem(
+                link='plugins:netbox_licenses:license_list',
+                link_text='License Management',
+                buttons=protected_resource_buttons
+            ),
+        )),
     ),
-    icon_class='mdi mdi-certificate'
+    icon_class='mdi mdi-shield-lock'
 )
