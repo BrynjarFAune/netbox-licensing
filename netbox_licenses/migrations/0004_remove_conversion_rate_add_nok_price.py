e881bc20c6e7c63a75bf8969a26ef061837234ca
Brynjar <brynjarfaune@gmail.com>
2025-10-06 08:52:33 +0200

    fix: update migration 0004 dependency to 0001_initial_complete

--- netbox_licenses/migrations/0004_remove_conversion_rate_add_nok_price.py
+++ netbox_licenses/migrations/0004_remove_conversion_rate_add_nok_price.py
@@ -3,6 +3,6 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('netbox_licenses', '0003_add_dkk_currency'),
+        ('netbox_licenses', '0001_initial_complete'),
     ]
     operations = []
